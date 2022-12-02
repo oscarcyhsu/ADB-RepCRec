40172ceec491f6d109d2ca2abb8cb628f39b967a
Yang <csjinyang0611@gmail.com>
2022-12-02 17:00:36 -0500

    print lock conflict event

--- main.py
+++ main.py
@@ -312,18 +312,20 @@ class TransactionManager():
     def write(self, transactionName: str, x: str, val: str):
         assert(transactionName in self.transactions)
         T = self.transactions[transactionName]
-
-        if not self.__getLock(T, x, "W"):
-            return False
+        
+        aquiredLock = self.__getLock(T, x, "W")
 
         hasDeadLock, victim = self.lockTable.checkDeadLock()
         while hasDeadLock:
             print(f"Deadlock detected, victim is {victim.name}")
             self.abortTransaction(victim)
             hasDeadLock, victim = self.lockTable.checkDeadLock()
-
-        if T.name not in self.transactions:
+        
+        if not aquiredLock:
             return False
+        if T.name not in self.transactions: # checkDeadLock abort T
+            return False
+
         T.variableFinalValues[x] = val
         return True
 
@@ -381,14 +383,20 @@ class TransactionManager():
         # TODO: scan instructionBuffer to see if any waiting instructions can now be run (if yes, they should be RO reads)
         pass
 
-
+    
     def __getLock(self, T: Transaction, x: str, lockType: str) -> bool:
         if lockType == "R":
             aquiredLock, blockingTransaction = self.lockTable.getReadLock(T, x)
         else:
             aquiredLock, blockingTransaction = self.lockTable.getWriteLock(T, x)
-        T.locks[x] = aquiredLock
-        return True
+        
+        if not aquiredLock:
+            print(f"Transaction {T.name} wait for transaction {blockingTransaction.name} " 
+                   "because of lock conflict")
+        else:
+            T.locks[x] = aquiredLock
+
+        return aquiredLock is not None
 
 
 
